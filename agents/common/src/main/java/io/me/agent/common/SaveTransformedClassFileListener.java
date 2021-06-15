74a0ea08ea1918f185db260fdd57d55e31b9d6a0
wangk <ke.wang8@amh-group.com>
2021-06-15 11:04:41 +0800

    delete tmp generated class file

--- agents/common/src/main/java/io/me/agent/common/SaveTransformedClassFileListener.java
+++ agents/common/src/main/java/io/me/agent/common/SaveTransformedClassFileListener.java
@@ -7,6 +7,8 @@ import net.bytebuddy.utility.JavaModule;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class SaveTransformedClassFileListener implements AgentBuilder.Listener {
     @Override
@@ -18,6 +20,31 @@ public class SaveTransformedClassFileListener implements AgentBuilder.Listener {
     public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded, DynamicType dynamicType) {
         try {
             File codeSavePath = new File(System.getProperty("user.dir") + File.separator + "bytecode");
+            Files.walkFileTree(Paths.get(codeSavePath.getPath()), new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+                    Files.deleteIfExists(file);
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.deleteIfExists(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
             if (!codeSavePath.exists() && codeSavePath.mkdir()) {
                 dynamicType.saveIn(codeSavePath);
             }
@@ -28,12 +55,12 @@ public class SaveTransformedClassFileListener implements AgentBuilder.Listener {
 
     @Override
     public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded) {
-
+        System.out.println("ignore transform this class:"+typeDescription.getName()+" loaded:"+loaded);
     }
 
     @Override
     public void onError(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded, Throwable throwable) {
-
+        System.out.println("transform class " + typeName + "error, cause by " + throwable.getMessage());
     }
 
     @Override
